48f9e38a59740409187430fec90f8e01cca3e514
James Andersen <james.andersen@cynergysystems.com>
2011-10-26 23:35:14 -0600

    Added icons and some additional styling

--- js/script.js
+++ js/script.js
@@ -1,31 +1,138 @@
-/* Author:
-
+/* Author: James Andersen
 */
 
 window.MF = (function()
 {
+	var Modes = {
+		STARTUP : 0,
+		PRACTICE : 1,
+		TIMED : 2
+	};
+	
+	var TEST_SECONDS = 120;
+	var NUM_QUESTIONS = 30;
+	
 	var maxNumber = 12;
+	var questionIndex = 0;
 	var currentAnswer;
+	var mode = Modes.STARTUP;
+	var nextQuestionId = "question1";
+	var timerId = null;
+	var testStartTime = null;
+	
+	var startPractice = function()
+	{
+		mode = Modes.PRACTICE;
+		$('#startup').css('display', 'none');
+		$('#return').css('visibility', 'visible');
+		$('.question').css('visibility', 'visible');
+		nextQuestion();
+	};
+	
+	var startTimed = function()
+	{
+		mode = Modes.TIMED;
+		$('#startup').css('display', 'none');
+		$('#progress').css('visibility', 'visible');
+		//$('#return').css('visibility', 'visible');
+		$('.submit').css('visibility', 'visible');
+		$('.question').css('visibility', 'visible');
+		
+		// setup the question progress bar
+		questionIndex = -1;
+		var questions = $('#questions');
+		questions.empty();
+		for(var i = 0; i < NUM_QUESTIONS; i++)
+		{
+			var q = $('<div class="' + (i === 0 ? 'next' : 'next') + '"/>');
+			questions.append(q);
+		}
+		
+		// start the timer
+		testStartTime = +new Date();
+		$('#elapsed').height('0%');
+		timerId = setInterval(onTick, 500);
+		nextQuestion();
+	};
+	
+	var onTick = function()
+	{
+		var elapsed = (+new Date()) - testStartTime;
+		if(elapsed > (TEST_SECONDS * 1000))
+		{
+			clearTimeout(timerId);
+		}
+		else
+		{
+			var pctElapsed = (elapsed / (TEST_SECONDS * 1000)) * 100;
+			$('#elapsed').height(pctElapsed + '%');
+		}
+	}
+	
+	var endTest = function()
+	{
+		clearTimeout(timerId);
+		var numCorrect = $('#questions').find('.correct').length;
+		alert('Done!\n' + numCorrect + ' of ' + NUM_QUESTIONS + ' correct');
+		returnToStartup();
+	};
+	
+	var returnToStartup = function()
+	{
+		$('#return').css('visibility', 'hidden');
+		$('#progress').css('visibility', 'hidden');
+		$('#startup').css('display', 'block');
+		$('.question').css('visibility', 'hidden');
+		$('.submit').css('visibility', 'hidden');
+		clearTimeout(timerId);
+	};
 	
 	var nextQuestion = function()
 	{
+		if(mode === Modes.TIMED && questionIndex >= 0)
+		{
+			$('#questions').find('div').eq(questionIndex).toggleClass('current');
+		}
+		
+		questionIndex++;
+		
+		if(mode === Modes.TIMED)
+		{
+			if(questionIndex === NUM_QUESTIONS)
+			{
+				endTest();
+			}
+			else
+			{
+				$('#questions').find('div').eq(questionIndex).toggleClass('current');
+			}
+		}
+		
 		var num1 = Math.floor(Math.random() * maxNumber + 1);
 		var num2 = Math.floor(Math.random() * maxNumber + 1);
 		
 		currentAnswer = num1 + num2;
 		
-		$('#num1').text(num1);
-		$('#num2').text(num2);
-	}
+		$('#' + nextQuestionId).find('.num1').text(num1);
+		$('#' + nextQuestionId).find('.num2').text(num2);
+	};
 	
 	var clearResponse = function()
 	{
-		$('#response').val('');
+		$('.response').val('');
 	};
 	
 	var responseUpdated = function(response)
 	{
-		if(parseInt(response) === currentAnswer)
+		var correct = parseInt(response) === currentAnswer;
+		if(mode === Modes.TIMED)
+		{
+			var questions = $('#questions');
+			questions.find('div').eq(questionIndex).toggleClass('next');
+			questions.find('div').eq(questionIndex).toggleClass(correct ? 'correct' : 'incorrect');
+		}
+			
+		if(correct || mode === Modes.TIMED)
 		{
 			clearResponse();
 			nextQuestion();
@@ -43,18 +150,18 @@ window.MF = (function()
 	
 	var init = function()
 	{
-		nextQuestion();
-  
-	  $('#next').click(nextQuestion);
+		$('#next').click(nextQuestion);
+		$('#practice').click(startPractice);
+		$('#timed').click(startTimed);
+		$('#return').click(returnToStartup);
 	  
-	  var response = $('#response');
+		var response = $('.response');
 	  
-	  response.keypress(function(event) {
+		response.keypress(function(event) {
 		  return MF.isValidResponseKey(event.keyCode);
 		});
 	
-	  
-	  response.keyup(function(event) {
+		response.keyup(function(event) {
 		  if (event.keyCode == '27') {
 		     clearResponse();
 		  }
@@ -62,9 +169,19 @@ window.MF = (function()
 		  {
 		  	// do nothing
 		  }
+		  else if(event.keyCode === 13)
+		  {
+			if(mode === Modes.TIMED)
+			{
+				responseUpdated($(this).val());
+			}
+		  }
 		  else // assuming only valid numeric input at this point
 		  {
-		  	responseUpdated($(this).val());
+		  	if(mode !== Modes.TIMED)
+			{
+				responseUpdated($(this).val());
+			}
 		  }
 		});
 		
@@ -73,6 +190,10 @@ window.MF = (function()
 		});
 		
 		response.focus();
+		
+		$('#submit').click(function(event){
+			responseUpdated($('#' + nextQuestionId).find('.response').val());
+		});
 	};
 	
 	return {
