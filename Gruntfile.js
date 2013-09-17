577687b5fce6bea889b65939f1297f396474c8a2
James Andersen <james.j.andersen@gmail.com>
2013-09-17 07:18:24 -0600

    using bower/grunt for modernizr and respond.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*global module:false*/
 module.exports = function (grunt) {
-
+    'use strict';
     // Project configuration.
     grunt.initConfig({
         // Metadata.
@@ -13,8 +13,14 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: 'bower_components/jquery/',
-                        src: ['jquery*.*'],
+                        src: ['jquery.*'],
                         dest: 'js/libs/jquery/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/respond/',
+                        src: ['respond.min.js'],
+                        dest: 'js/libs/respond/'
                     }
                 ]
             }
@@ -71,8 +77,11 @@ module.exports = function (grunt) {
             gruntfile: {
                 src: 'Gruntfile.js'
             },
-            lib_test: {
-                src: ['js/app/**/*.js', '_test/tests/**/*.js']
+            app: {
+                src: 'js/app/**/*.js'
+            },
+            tests: {
+                src: '_test/tests/**/*.js'
             }
         },
         qunit: {
@@ -84,9 +93,63 @@ module.exports = function (grunt) {
                 tasks: ['jshint:gruntfile']
             },
             lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'qunit']
+                files: '<%= jshint.tests.src %>',
+                tasks: ['jshint:tests', 'qunit']
+            },
+            app: {
+                files: ['<%= jshint.gruntfile.src %>', '<%= jshint.app.src %>', 'index.html', 'css/*.less'],
+                tasks: ['copy', 'less', 'modernizr']
             }
+        },
+        modernizr: {
+
+            // [REQUIRED] Path to the build you're using for development.
+            "devFile": "bower_components/modernizr/modernizr.js",
+
+            // [REQUIRED] Path to save out the built file.
+            "outputFile": "js/libs/modernizr/modernizr-custom.js",
+
+            // Based on default settings on http://modernizr.com/download/
+            "extra": {
+                "shiv": true,
+                "printshiv": false,
+                "load": true,
+                "mq": false,
+                "cssclasses": true
+            },
+
+            // Based on default settings on http://modernizr.com/download/
+            "extensibility": {
+                "addtest": false,
+                "prefixed": false,
+                "teststyles": false,
+                "testprops": false,
+                "testallprops": false,
+                "hasevents": false,
+                "prefixes": false,
+                "domprefixes": false
+            },
+
+            // By default, source is uglified before saving
+            "uglify": true,
+
+            // Define any tests you want to implicitly include.
+            "tests": [],
+
+            // By default, this task will crawl your project for references to Modernizr tests.
+            // Set to false to disable.
+            "parseFiles": true,
+            
+            // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
+            // You can override this by defining a "files" array below.
+            "files" : ["js/app/**/*", "css/styles.min.css", "index.html"],
+
+            // When parseFiles = true, matchCommunityTests = true will attempt to
+            // match user-contributed tests.
+            "matchCommunityTests": false,
+
+            // Have custom Modernizr tests? Add paths to their location here.
+            "customTests": []
         }
     });
 
@@ -98,6 +161,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-modernizr");
 
     // Default task.
     grunt.registerTask('default', ['less', 'copy', 'jshint:gruntfile', 'uglify']);
