82ea58b8bcb12d7122a3c1b5c24ffe5e38e6a97b
James Andersen <james.j.andersen@gmail.com>
2013-09-19 22:28:51 -0600

    Converting some CSS to LESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,6 @@ module.exports = function (grunt) {
         less: {
             production: {
                 options: {
-                    paths: ["css/"],
                     yuicompress: true
                 },
                 files: {
