68fe6444099d997f61a69d604259e95fb89a540d
James Andersen <james.andersen@universalmind.com>
2016-03-11 19:48:22 -0500

    Add prod version

--- src/app/question/question.ts
+++
@@ -1,8 +0,0 @@
-import {Operation} from './question-enums';
-
-export class Question {
-   public isCorrect: boolean;
-   
-   constructor(public num1: number, public num2: number, public operation: Operation, public answer: number) {
-   }
-}
